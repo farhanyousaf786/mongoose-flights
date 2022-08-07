a12dc5ae551530332d1feb6c45c136f2f2c90f69
Farhan Yousaf <farhanyousaf1996@gmail.com>
2022-08-07 18:20:42 -0400

    get response and render it to All Air Line List

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,45 +1,38 @@
 const Flight = require('../models/flights');
 
-
 module.exports = {
     new: newFlight,
     create,
+    index,
 }
 
 function newFlight(req, res) {
-
     res.render('flights/new.ejs');
+}
 
+
+
+function index(req, res) {
+
+    Flight.find({}, function (err, allOfTheFlightsInTheDatabase) {
+        console.log(allOfTheFlightsInTheDatabase, "<<-- All the flights");
+        if (err) {
+            res.send("We have error while trying to find movies in db");
+        }
+        res.render('flights/index.ejs', {
+            flights: allOfTheFlightsInTheDatabase,
+        });
+    });
 }
 
 function create(req, res) {
-
     console.log(req.body, "< -- this is we enter in form")
-
-
     Flight.create(req.body, function (err, flightDocCreateInDB) {
-
         if (err) {
-
             console.log(err, "<---  this is error bro");
             return res.render('flights/new.ejs');
-
         }
-
-
         console.log(flightDocCreateInDB, "< -- this fligh in DB bro");
-
         res.send("Response from create function for movie");
-
-
-
-
     })
-
-
-
-
-
-
-
-} 
+}
--- routes/flights.js
+++ routes/flights.js
@@ -3,11 +3,10 @@ var router = express.Router();
 const flightController = require('../controllers/flights');
 /* GET users listing. */
 
+
+router.get('/', flightController.index);
+
 router.get('/new', flightController.new);
 
-
-
 router.post('/', flightController.create);
-
-
 module.exports = router;
