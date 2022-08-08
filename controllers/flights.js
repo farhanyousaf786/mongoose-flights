de05bfb41c118009221f5b54849741e26fa491b3
Farhan Yousaf <farhanyousaf1996@gmail.com>
2022-08-08 18:42:48 -0400

    added details option

--- controllers/flights.js
+++ controllers/flights.js
@@ -4,12 +4,21 @@ module.exports = {
     new: newFlight,
     create,
     index,
+    show,
 }
 
 function newFlight(req, res) {
     res.render('flights/new.ejs');
 }
 
+
+function show(req, res,) {
+    Flight.findById(req.params.id, function (err, flight) {
+        res.render('flights/show', {
+            title: "Flight Detail", flight
+        });
+    });
+}
 
 function index(req, res) {
     Flight.find({}, function (err, allOfTheFlightsInTheDatabase) {
@@ -28,7 +37,7 @@ function create(req, res) {
     console.log(req.body, "< -- this is we enter in form")
     Flight.create(req.body, function (err, flightDocCreateInDB) {
         if (err) {
-            console.log(err, "<---  this is error bro");
+            console.log(err, "<---  this is error");
             return res.render('flights/new.ejs');
         }
         console.log(flightDocCreateInDB, "< -- this fligh in DB bro");
