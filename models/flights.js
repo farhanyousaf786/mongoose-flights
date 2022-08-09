39e429683e5ed5934cf831136cbf356e8ae37ced
Farhan Yousaf <farhanyousaf1996@gmail.com>
2022-08-09 01:55:37 -0400

    add destinations fields

--- models/flights.js
+++ models/flights.js
@@ -1,28 +1,37 @@
 const mongoose = require('mongoose');
 
+
+
+const destinationSchema = new mongoose.Schema({
+
+    airport: {
+        type: String,
+        defualt: 'DEN',
+        enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'],
+    },
+    arrival: Date
+
+})
+
+
 var oneYearFromNow = new Date();
-
-
 const flightSchema = new mongoose.Schema({
-
     airline: {
         type: String,
         enum: ['American', 'Delta', 'Southwest', 'United']
     },
-
     airport: {
         type: String,
         defualt: 'DEN',
         enum: ['ATL', 'DFW', 'DEN', 'LAX', 'SAN'],
     },
-
     flightNo: Number,
 
     departs: {
-    type: Date,
-    defualt: oneYearFromNow.setFullYear(oneYearFromNow.getFullYear() + 1),
-   },
-
+        type: Date,
+        defualt: oneYearFromNow.setFullYear(oneYearFromNow.getFullYear() + 1),
+    },
+    destinations: [destinationSchema],
 });
 
 
